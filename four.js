316a2ef532f501ad4178c460fa280e5ea607da8b
AAKASH HANDA <AAKASH HANDA>
2017-07-20 07:30:29 +0530

    A

---
+++ four.js
@@ -0,0 +1,39 @@
+var a = 15
+
+if(a%2==0){
+	console.log("number"+ a + "is even")
+}else{
+	console.log("number"+ a + "is odd")
+}
+
+
+var a = 15
+if(a%2==0){
+	console.log(a+ " is divisible by 2")
+}else if(a%3==0){
+	console.log(a + " is divisible by 3")
+}else{
+	console.log("number is not divisible")
+}
+
+var a = 30;
+if(a>20){
+	console.log("i am good")
+}
+else{
+	console.log("i am not good")
+}
+
+var a = 15
+if(a%2 ==0 || a%3 ==0){
+	console.log("number is divisible")
+}else{
+	console.log("number is not divisible")
+}
+
+var a = 15
+if(a%2 ==0 && a%3 ==0){
+	console.log("number is divisible")
+}else{
+	console.log("number is not divisible")
+}
