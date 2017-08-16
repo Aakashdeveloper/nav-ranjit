e40edba1925b74d61b4248e2ef91c513c9d47466
AAKASH HANDA <AAKASH HANDA>
2017-08-17 01:06:58 +0530

    A

---
+++ for.js
@@ -0,0 +1,45 @@
+for(i=0;i<10;i++){
+    console.log(i)
+}
+
+
+for(i=10;i>=0;i--){
+    console.log(i)
+}
+
+var a = "navneth"
+var name=""
+for(i=a.length;i>=0;i--){
+    name +=a[i]
+}
+console.log(name)
+
+for(i=0;i<=5;i++){
+	for(j=i;j<=5;j++){
+		console.log("*")
+	}
+}
+
+for(i=0;i<=5;i++){
+	for(j=i;j<=5;j++){
+		console.log("*")
+	}
+}
+
+var i=0
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+switch (name){
+    case "aakash":
+        console.log("tis is case1")
+        break;
+    case "manju":
+        console.log("tis is case2")
+        break;
+    default:
+        console.log("tis is default")
+    
+}
